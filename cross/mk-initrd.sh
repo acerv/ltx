4fe27a0c3995341575510f0b6b86386d2d00ff60
Richard Palethorpe <rpalethorpe@suse.com>
2023-06-05 12:55:41 +0100

    Add sysinfo dummy test and initrd script

---
+++ cross/mk-initrd.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/sh -eu
+
+rootdir=$(realpath $1)
+
+if [ -z $rootdir ]; then
+        echo "Expected directory as first positional parameter"
+        exit 1
+fi
+
+if [ ! -d $rootdir/bin ]; then
+        echo "Expected bin directory in $rootdir"
+        exit 1
+else
+        tests=$(find $rootdir/bin -type f -executable)
+        if [ -z $tests ]; then
+                echo "Expected tests in $rootdir/bin"
+                exit 1
+        fi
+fi
+
+if [ ! -x $rootdir/init ]; then
+        echo "Expected init executable (LTX) in $rootdir"
+fi
+
+bname=$(basename $rootdir)
+find $rootdir | cpio -v -H newc -o | gzip -n > $rootdir/../$bname.cpio.gz
